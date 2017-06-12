1d993595a846fda3b3cc1dfa27f72a75c94083ae
Дмитрий Середа <q3dm0@yahoo.com>
2017-06-12 11:31:36 +0300

    Изменения шаблона

--- sites/all/themes/insight/templates/html.tpl.php
+++ sites/all/themes/insight/templates/html.tpl.php
@@ -6,7 +6,8 @@
 		<meta content="telephone=no" name="format-detection">
 <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
+	<meta name="viewport" content="width=1320">
     
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
